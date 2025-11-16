0af89e72afe2f1fd7fc7dc6c65ed290df3ddf91e
Mykhailo Panasiuk <paskaworking19@gmail.com>
2025-11-16 16:42:29 +0200

    lab3-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+final List<String> pizza = <String>[
+  "Маргарита",
+  "М'ясна",
+  "Чотири сири",
+];
+
+final List<String> receptPizza = <String>[
+  "Тісто, томатний соус, моцарела, базилік",
+  "Бекон, мисливські ковбаски, шинка, сир",
+  "Чеддер, моцарела, фета, рокфор",
+];
 
 void main() {
   runApp(MyApp());
@@ -8,100 +21,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Pizza App',
       debugShowCheckedModeBanner: false,
-      title: "Stateful Counter Widget",
-      theme: ThemeData(primarySwatch: Colors.amber),
       home: Scaffold(
-        backgroundColor: Colors.amber[300],
-        appBar: AppBar(title: Text("Завдання 2")),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              HintLabel('Натисніть «-» для зменшення'),
-              SizedBox(height: 8.0),
-              CounterWidget(), // кастомний віджет
-              SizedBox(height: 8.0),
-              HintLabel('Натисніть «+» для збільшення'),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text("Завдання 3"),
         ),
+        body: ListDynamicPizza(),
       ),
     );
   }
 }
 
-// ----------------------------
-// Власний віджет HintLabel
-// ----------------------------
-class HintLabel extends StatelessWidget {
-  final String text;
-
-  const HintLabel(this.text);
-
+class ListDynamicPizza extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(color: Colors.amber[200]),
-      child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Text(
-          text,
-          style: TextStyle(color: Colors.grey[700]),
-        ),
-      ),
-    );
-  }
-}
-
-// ----------------------------
-// Власний кастомний CounterWidget
-// ----------------------------
-class CounterWidget extends StatefulWidget {
-  @override
-  _CounterWidgetState createState() => _CounterWidgetState();
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  int _count = 0;
-
-  void _increment() {
-    setState(() {
-      _count++;
-    });
-  }
-
-  void _decrement() {
-    setState(() {
-      _count--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.amber[600],
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            onPressed: _decrement,
-            icon: Icon(Icons.remove),
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: pizza.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: const Icon(Icons.local_pizza, size: 28),
+          title: Text(
+            pizza[index],
+            style: const TextStyle(fontSize: 22),
           ),
-          Text(
-            '$_count',
-            style: TextStyle(fontSize: 40),
-          ),
-          IconButton(
-            onPressed: _increment,
-            icon: Icon(Icons.add),
-          ),
-        ],
-      ),
+          trailing: const Icon(Icons.arrow_right),
+          subtitle: Text("Склад: ${receptPizza[index]}"),
+          onTap: () {
+            Fluttertoast.showToast(
+              msg: "Ви обрали: ${pizza[index]}",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 1,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              fontSize: 16.0,
+            );
+          },
+        );
+      },
     );
   }
 }
